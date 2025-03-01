7a12572257be620b6c7e3a3b939776188a692ae0
digijin <james@digijin.com>
2025-03-01 20:21:58 +1100

    proto noto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const isPrototype = process.env.NEXT_PUBLIC_IS_PROTO === "true";
+
   return (
     <div>
-      <h1>Parakeet</h1>
+      {isPrototype && <div className="notification-warning">This is a prototype</div>}
       <div>homepage content</div>
       <Link href="/about">Learn more about us</Link>
     </div>
