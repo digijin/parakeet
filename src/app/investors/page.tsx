f0eb918b0bde3032177a23702ab30585fed7825b
digijin <james@digijin.com>
2025-03-01 23:00:34 +1100

    cheeky investor link

---
+++ src/app/investors/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function InvestorsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Investors</h1>
+      <p>Welcome to the Investors page. We are currently looking for investors.</p>
+      <p>If you are interested, please contact Jesse at <a href="mailto:james@digijin.com" className="text-blue-500 underline">temp link</a>.</p>
+    </div>
+  );
+}
